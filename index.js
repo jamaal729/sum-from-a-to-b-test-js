6750f057887d1a38525920bf53dafd512c15c6ca
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-19 17:27:24 +0000

    Implemented recursion from a to b

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let total = 0;
+  let i = fromN;
+
+  // for loop
+  // for (let i = fromN; i <= toN; i++) {
+  //   console.log(total);
+  //   total += i;
+  // }
+  // return total;
+
+  // recursive 1
+  // if (i <= toN) {
+  //   total = i + sum(i + 1, toN);
+  //   console.log(i);
+  // }
+  // return total;
+
+  // recursive 2
+  if (i > toN) {
+    return total;
+  }
+  console.log(i);
+  return i + sum(i + 1, toN);
 }
 
 module.exports = sum;
+
+console.log(sum(3, 7));
